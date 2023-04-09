ab74d263339328b95ea8e50a9643f10deafe4a61
Vrihedd <pavel26neagu@gmail.com>
2023-04-09 13:03:34 +0300

    Curs 5 left at 0:50

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const express = require("express");
 app= express();
 console.log("Folder proiect", __dirname);
 
+app.set("view engine","ejs");
+
+app.use("/stuff",express.static(__dirname+"/stuff")); //scapam de 7000 de app.geturi
+
 app.get("/ceva", function(req, res){
     res.send("altceva");
 })
 
-app.get("/index.html", function(req, res){
-    res.sendFile(__dirname+"/index.html");
+app.get("/index", function(req, res){
+    res.render("pagini/index");
 })
 
 app.listen(8080);

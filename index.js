ad803b517fae0592e39aa89812b20fdd45da20c1
Vrihedd <pavel26neagu@gmail.com>
2023-05-09 20:06:21 +0300

    Lab 5 done, left at Etapa 4 Task 15

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const fs=require("fs");
-
+obGlobal={
+    obErori:null,
+    obImagini:null
+}
 app= express();
 console.log("Folder proiect", __dirname);
 
@@ -17,12 +20,60 @@ app.get(["/index","/","/home"], function(req, res){
 })
 
 app.get("/*", function(req,res){
-    res.render("pagini"+req.url);
+    console.log(req.url);
+    res.render("pagini"+req.url, function(err, rezRandare){
+        if(err){
+            console.log(err);
+            if(err.message.startsWith("Failed to lookup view"))
+            {
+                afiseazaEroare(res, 404);
+            }
+            else
+            {
+                afiseazaEroare(res);
+            }
+        }
+        else{
+            console.log(rezRandare);
+            res.send(rezRandare);
+        }
+    });
+    
 
 })
 
 function initializeazaErori(){
-
+    var continut=fs.readFileSync(__dirname+"/stuff/json/erori.json").toString("utf-8");
+    console.log(continut);
+    obGlobal.obErori=JSON.parse(continut);
+    let vErori=obGlobal.obErori.info_erori;
+    //for(let i=0;i<vErori.length;i++)//vErori[i]
+    for(let eroare of vErori){
+        eroare.imagine="/" + obGlobal.obErori.cale_baza + "/" + eroare.imagine;
+    }
 }
+initializeazaErori()
+
+function afiseazaEroare(res, _identificator, _titlu, _text, _imagine){
+    let vErori=obGlobal.obErori.info_erori;
+    let eroare=vErori.find(function(elem){return elem.identificator == _identificator});
+    if(eroare){
+        let titlu=_titlu || eroare.titlu;
+        let text=_text || eroare.text;
+        let imagine=_imagine || eroare.imagine;
+        if(eroare.status)
+            res.status(eroare.identificator).render("pagini/eroare",{titlu:eroare.titlu, text:eroare.text, imagine:eroare.imagine});
+        else
+            res.render("pagini/eroare",{titlu:eroare.titlu, text:eroare.text, imagine:eroare.imagine1});
+    }
+    else
+        {
+            var errDef=obGlobal.obErori.eroare_default;
+            res.render("pagini/eroare",{titlu:errDef.titlu, text:errDef.text, imagine:errDef.imagine});
+    }
+        
+}
+
+
 app.listen(8080);
 console.log("Serverul a pornit");
